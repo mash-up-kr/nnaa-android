be4411caf4906a22f6beff933112e6cf9f389f7d
Seulgi Kim <7d4z7l@gmail.com>
2019-11-23 20:01:50 +0900

    ADD Splash Activity - Need Review

--- app/src/main/java/com/mashup/nnaa/MainActivity.java
+++ app/src/main/java/com/mashup/nnaa/MainActivity.java
@@ -2,6 +2,7 @@ package com.mashup.nnaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
