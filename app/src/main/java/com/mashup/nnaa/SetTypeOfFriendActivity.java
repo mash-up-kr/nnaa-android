f514196f29a044af41fcc6b676f5a14effa75437
Seulgi Kim <7d4z7l@gmail.com>
2019-11-23 23:59:06 +0900

    ADD Multi-ligual Support

--- app/src/main/java/com/mashup/nnaa/SetTypeOfFriendActivity.java
+++ app/src/main/java/com/mashup/nnaa/SetTypeOfFriendActivity.java
@@ -2,10 +2,15 @@ package com.mashup.nnaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SetTypeOfFriendActivity extends AppCompatActivity {
 
@@ -14,16 +19,20 @@ public class SetTypeOfFriendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_type_of_friend);
 
-
         //Spinner
-        Spinner friendTypeSpinner = (Spinner)findViewById(R.id.spnTypeOfFriend);
+        Spinner friendTypeSpinner = findViewById(R.id.spnTypeOfFriend);
         ArrayAdapter friendTypeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.type_of_friend,android.R.layout.simple_spinner_item);
         friendTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         friendTypeSpinner.setAdapter(friendTypeAdapter);
 
         //NextButton
-        Button nextbtninTOF = (Button)findViewById(R.id.nextbtninTOF);
+        Button nextbtninTOF = findViewById(R.id.nextbtninTOF);
 
+        Resources res = getResources();
+        String yourname = "Seulgi";
+        String iswhom = String.format(res.getString(R.string.youaremy), yourname);
+        TextView youaremy = findViewById(R.id.youaremy);
+        youaremy.setText(iswhom);
     }
 }
--- app/src/main/java/com/mashup/nnaa/SplashActivity.java
+++ app/src/main/java/com/mashup/nnaa/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
 
-        }, 3000);
+        }, 2000);
 
 
 
