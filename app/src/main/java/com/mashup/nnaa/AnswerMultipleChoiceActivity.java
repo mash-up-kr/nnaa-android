79dc084f48a0c9a2629c74f725a3b77f48cf3126
Seulgi Kim <7d4z7l@gmail.com>
2020-01-11 22:57:51 +0900

    Set OnClickListener on multiple-choice buttons Set OnClickListener on left button

--- app/src/main/java/com/mashup/nnaa/AnswerMultipleChoiceActivity.java
+++ app/src/main/java/com/mashup/nnaa/AnswerMultipleChoiceActivity.java
@@ -5,13 +5,10 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AnswerMultipleChoiceActivity extends AppCompatActivity {
-    TextView stopBtnText;
     ImageButton stopBtn;
     LinearLayout leftbtn;
     RelativeLayout rightbtn;
@@ -22,31 +19,32 @@ public class AnswerMultipleChoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_multiple_choice);
 
-        //stopBtnText = findViewById(R.id.stop_answer_multiple_text);
-        //stopBtn = findViewById(R.id.stop_answer_multiple);
-
+        stopBtn = findViewById(R.id.stop_answer_multiple_btn);
         leftbtn = findViewById(R.id.undo_btn_in_multiple_chioce);
         rightbtn = findViewById(R.id.next_btn_in_multiple_chioce);
-
-        leftbtn.setOnClickListener((view) -> finish());
-
         multiple_choice[0] = findViewById(R.id.multiple_choice_btn_A);
         multiple_choice[1] = findViewById(R.id.multiple_choice_btn_B);
         multiple_choice[2] = findViewById(R.id.multiple_choice_btn_C);
         multiple_choice[3] = findViewById(R.id.multiple_choice_btn_D);
 
+        leftbtn.setOnClickListener((view) -> finish());
         for(int i=0;i<4;i++)
             multiple_choice[i].setOnClickListener(new MultipleChoiceListener(this));
     }
 }
 
 class MultipleChoiceListener implements View.OnClickListener {
-    AppCompatActivity parent;
-    MultipleChoiceListener(AppCompatActivity app) {
+    AnswerMultipleChoiceActivity parent;
+    MultipleChoiceListener(AnswerMultipleChoiceActivity app) {
         parent = app;
     }
     @Override
     public void onClick(View view) {
-        Toast.makeText(parent.getApplicationContext(), view.getId()+"is clicked",Toast.LENGTH_SHORT).show();
+        if(view.isPressed()) {
+            for(int i=0;i<4;i++)
+                parent.multiple_choice[i].setSelected(false);
+            view.setSelected(true);
+           // Toast.makeText(parent.getApplicationContext(), view.getId() + "is selected", Toast.LENGTH_SHORT).show();
+        }
     }
 }
