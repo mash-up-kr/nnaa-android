e737a8b6609f94c057ac4de19ddc91d010a56a84
Jujin <jujin@jujinkim.com>
2020-01-13 00:06:54 +0900

    fix: add GsonConverter and Http Interceptor at Retrofit builder

--- app/src/main/java/com/mashup/nnaa/network/RetrofitHelper.java
+++ app/src/main/java/com/mashup/nnaa/network/RetrofitHelper.java
@@ -1,20 +1,41 @@
 package com.mashup.nnaa.network;
 
+import com.mashup.nnaa.BuildConfig;
+import com.mashup.nnaa.network.model.QuestionnaireDto;
 import com.mashup.nnaa.network.model.UserInfo;
 
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitHelper {
-    private Retrofit retrofit = null;
+    private Retrofit retrofit;
+    public final String ENTRY_URL = "http://52.78.191.221:8080/";
 
     // singleton
-    private RetrofitHelper() { retrofit = new Retrofit.Builder().baseUrl(ENTRY_URL).build(); }
     private static final RetrofitHelper _instance = new RetrofitHelper();
     public static RetrofitHelper getInstance() { return _instance; }
 
-    public final String ENTRY_URL = "http://52.78.191.221:8080/";
+    private RetrofitHelper() {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(ENTRY_URL)
+                .addConverterFactory(GsonConverterFactory.create());
+
+        // add interceptor on DEBUG mode
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+            builder.client(client);
+        }
+
+        retrofit = builder.build();
+    }
 
     public void getUserInfo(Callback<UserInfo> callback) {
         UserControllerService service = retrofit.create(UserControllerService.class);
