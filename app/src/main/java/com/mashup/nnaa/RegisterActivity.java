972f79e7b28fb433f6267fad356b2402aae866b8
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-02-26 18:00:11 +0900

    로그인 회원가입 디자인 입히기

--- app/src/main/java/com/mashup/nnaa/RegisterActivity.java
+++ app/src/main/java/com/mashup/nnaa/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -16,101 +17,74 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText email_edit, password_edit, password_edit_confirm;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_register);
 
-        email_edit = findViewById(R.id.email_edit);
-        password_edit = findViewById(R.id.password_edit);
-        password_edit_confirm = findViewById(R.id.password_edit_confirm);
-        Button btn_signIn_done = findViewById(R.id.btn_signIn_done);
-        Button btn_signIn_cancel = findViewById(R.id.btn_signIn_cancel);
+        EditText edit_name = findViewById(R.id.edit_name);
+        EditText edit_email = findViewById(R.id.edit_email);
+        EditText edit_email_address = findViewById(R.id.edit_email_address);
+        EditText edit_password_confirm = findViewById(R.id.edit_password_confirm);
+        EditText edit_password = findViewById(R.id.edit_password);
 
-        password_edit_confirm.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+        ImageView img_register_close = findViewById(R.id.img_register_close);
 
-            }
+        img_register_close.setOnClickListener(view -> {
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
-                String password = password_edit.getText().toString();
-                String confirm = password_edit_confirm.getText().toString();
-
-                if(password.equals(confirm)) {
-                    password_edit.setBackgroundColor(Color.GREEN);
-                    password_edit_confirm.setBackgroundColor(Color.GREEN);
-                }
-                else {
-                    password_edit.setBackgroundColor(Color.RED);
-                    password_edit_confirm.setBackgroundColor(Color.RED);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-        btn_signIn_done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(email_edit.getText().toString().length()==0) {
-                    Toast.makeText(SignInActivity.this, "Email을 입력해주세요!", Toast.LENGTH_SHORT).show();
-                    email_edit.requestFocus();
-                    return;
-                }
-
-                if(password_edit.getText().toString().length()==0) {
-                    Toast.makeText(SignInActivity.this, "Password를 입력해주세요!", Toast.LENGTH_SHORT).show();
-                    password_edit.requestFocus();
-                    return;
-                }
-                if(password_edit_confirm.getText().toString().length()==0) {
-                    Toast.makeText(SignInActivity.this, "Password Confirm을 입력해주세요!", Toast.LENGTH_SHORT).show();
-                    password_edit_confirm.requestFocus();
-                    return;
-                }
-
-                if(!password_edit.getText().toString().equals(password_edit_confirm.getText().toString())) {
-                    Toast.makeText(SignInActivity.this, "Password가 일치하지 않습니다!",Toast.LENGTH_SHORT).show();
-                    password_edit.setText("");
-                    password_edit_confirm.setText("");
-                    password_edit.requestFocus();
-                    return;
-                }
-
-                Intent doneIntent = new Intent(SignInActivity.this, LoginActivity.class);
-                startActivity(doneIntent);
-            }
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(RegisterActivity.this);
+            alertDialogBuilder.setMessage("회원가입을 취소하시겠습니까?");
+            alertDialogBuilder.setPositiveButton("네", (dialogInterface, i) -> {
+                Toast.makeText(RegisterActivity.this, "회원가입을 취소하였습니다!", Toast.LENGTH_SHORT).show();
+                Intent backIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(backIntent);
+            });
+            alertDialogBuilder.setNegativeButton("취소", (dialogInterface, i) -> Toast.makeText(RegisterActivity.this, "회원가입을 작성해주세요!", Toast.LENGTH_SHORT).show());
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
         });
 
-        btn_signIn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SignInActivity.this);
-                alertDialogBuilder.setMessage("회원가입을 취소하시겠습니까?");
-                alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(SignInActivity.this, "회원가입을 취소하였습니다.", Toast.LENGTH_SHORT).show();
-                        Intent backIntent = new Intent(SignInActivity.this, LoginActivity.class);
-                        startActivity(backIntent);
-                    }
-                });
-                alertDialogBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(SignInActivity.this, "회원가입을 작성해주세요!", Toast.LENGTH_SHORT).show();
+        Button btn_register = findViewById(R.id.btn_register);
 
-                    }
-                });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
+        btn_register.setOnClickListener(view -> {
+            if (edit_name.getText().toString().length() == 0) {
+                Toast.makeText(RegisterActivity.this, "이름을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                edit_name.requestFocus();
+                return;
             }
+
+            if (edit_email.getText().toString().length() == 0) {
+                Toast.makeText(RegisterActivity.this, "email을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                edit_email.requestFocus();
+                return;
+            }
+            if (edit_email_address.getText().toString().length() == 0) {
+                Toast.makeText(RegisterActivity.this, "email 주소를 입력해주세요!", Toast.LENGTH_SHORT).show();
+                edit_email_address.requestFocus();
+                return;
+            }
+            if (edit_password.getText().toString().length() == 0) {
+                Toast.makeText(RegisterActivity.this, "비밀번호를 입력해주세요!", Toast.LENGTH_SHORT).show();
+                edit_password.requestFocus();
+                return;
+            }
+            if (edit_password_confirm.getText().toString().length() == 0) {
+                Toast.makeText(RegisterActivity.this, "비밀번호 확인란을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                edit_password_confirm.requestFocus();
+                return;
+            }
+
+            if (!edit_password.getText().toString().equals(edit_password_confirm.getText().toString())) {
+                Toast.makeText(RegisterActivity.this, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                edit_password.setText("");
+                edit_password_confirm.setText("");
+                edit_password.requestFocus();
+                return;
+            }
+
+            Intent doneIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(doneIntent);
+
         });
     }
 }
