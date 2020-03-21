36302fcc50ac92ac0517f49b02513311fd3fb3d9
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-03-21 14:10:32 +0900

    질문 받아오기 & 질문 삭제

--- app/src/main/java/com/mashup/nnaa/data/QuestionItem.java
+++ app/src/main/java/com/mashup/nnaa/data/QuestionItem.java
@@ -1,28 +1,33 @@
 package com.mashup.nnaa.data;
 
-public class QuestionItem {
+import java.io.Serializable;
 
-    String questionary;
-    String mainQ;
-
-    public String getQuestionary() {
-        return questionary;
-    }
-
-    public void setQuestionary(String questionary) {
-        this.questionary = questionary;
-    }
-
-    public String getMainQ() {
-        return mainQ;
-    }
-
-    public void setMainQ(String mainQ) {
-        this.mainQ = mainQ;
-    }
+public class QuestionItem implements Serializable {
+    String qeustion_num;
+    String question_content;
 
     public QuestionItem() {
-        this.questionary = questionary;
-        this.mainQ = mainQ;
+
+    }
+
+    public String getQeustion_num() {
+        return qeustion_num;
+    }
+
+    public void setQeustion_num(String qeustion_num) {
+        this.qeustion_num = qeustion_num;
+    }
+
+    public String getQuestion_content() {
+        return question_content;
+    }
+
+    public void setQuestion_content(String question_content) {
+        this.question_content = question_content;
+    }
+
+    public QuestionItem(String qeustion_num, String question_content) {
+        this.qeustion_num = qeustion_num;
+        this.question_content = question_content;
     }
 }
