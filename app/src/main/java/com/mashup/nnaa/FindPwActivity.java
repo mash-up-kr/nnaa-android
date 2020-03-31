3da6cab8a139d7f23e4ab52b16aeddf424b60d62
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-03-31 23:26:01 +0900

    send email

--- app/src/main/java/com/mashup/nnaa/FindPwActivity.java
+++ app/src/main/java/com/mashup/nnaa/FindPwActivity.java
@@ -28,16 +28,24 @@ public class FindPwActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        String email = editFind.getText().toString();
+
         btnFind.setOnClickListener(view -> {
             if (editFind.getText().toString().isEmpty()) {
                 Toast.makeText(FindPwActivity.this, "가입하신 이메일을 입력해주세요!", Toast.LENGTH_SHORT).show();
             } else {
                 progressDialog.setMessage("메일을 보내는중입니다..");
                 progressDialog.show();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "NNAA 비밀번호 찾기메일입니다.");
+                intent.putExtra(Intent.EXTRA_TEXT, "비밀번호");
+                startActivity(intent);
                 Toast.makeText(FindPwActivity.this, "이메일로 비밀번호를 보냈습니다!", Toast.LENGTH_SHORT).show();
                 btnFind.setBackgroundColor(Color.BLUE);
             }
-            //progressDialog.dismiss();
+            progressDialog.dismiss();
         });
     }
 }
