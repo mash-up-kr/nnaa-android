70eda46d8a6b5d5b03ecf86196688170b689a607
SeungYooon <40010002+SeungYooon@users.noreply.github.com>
2020-04-01 11:14:30 +0900

    Update FindPwActivity.java

--- app/src/main/java/com/mashup/nnaa/FindPwActivity.java
+++ app/src/main/java/com/mashup/nnaa/FindPwActivity.java
@@ -30,22 +30,27 @@ public class FindPwActivity extends AppCompatActivity {
 
         String email = editFind.getText().toString();
 
-        btnFind.setOnClickListener(view -> {
+         btnFind.setOnClickListener(view -> {
             if (editFind.getText().toString().isEmpty()) {
                 Toast.makeText(FindPwActivity.this, "가입하신 이메일을 입력해주세요!", Toast.LENGTH_SHORT).show();
             } else {
-                progressDialog.setMessage("메일을 보내는중입니다..");
-                progressDialog.show();
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("plain/text");
                 intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
                 intent.putExtra(Intent.EXTRA_SUBJECT, "NNAA 비밀번호 찾기메일입니다.");
                 intent.putExtra(Intent.EXTRA_TEXT, "비밀번호");
                 startActivity(intent);
-                Toast.makeText(FindPwActivity.this, "이메일로 비밀번호를 보냈습니다!", Toast.LENGTH_SHORT).show();
+                Log.v("@@@@@@@@@@@","mail");
                 btnFind.setBackgroundColor(Color.BLUE);
+
+                progressDialog.setMessage("메일을 보내는중입니다..");
+                progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal);
+                progressDialog.show();
+
+                Toast.makeText(FindPwActivity.this, "이메일로 비밀번호를 보냈습니다!", Toast.LENGTH_SHORT).show();
             }
-            progressDialog.dismiss();
+
+           // progressDialog.dismiss();
         });
     }
 }
