5a02d3fe80d72eb83e69c62ad980b2fd13f907e0
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-04-05 22:35:31 +0900

    complete make question

--- app/src/main/java/com/mashup/nnaa/MakeQuestionActivity.java
+++ app/src/main/java/com/mashup/nnaa/MakeQuestionActivity.java
@@ -10,12 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 import com.mashup.nnaa.network.RetrofitHelper;
 import com.mashup.nnaa.network.model.NewQuestionDto;
+import com.mashup.nnaa.question.QuestionActivity;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,11 +25,11 @@ import retrofit2.Response;
 
 public class MakeQuestionActivity extends AppCompatActivity {
 
-    private NewQuestionDto newQu = new NewQuestionDto();
     Button CustomDone;
-    ImageButton Custom_J, Custom_G, Custom_OX, Custom_J_Blue, Custom_G_Blue, Custom_OX_Blue;
-    TextView txtJ, txtG, txtOX, txt_category;
+    ImageButton Custom_OX, Custom_J_Blue, Custom_G_Blue, Custom_OX_Blue, Custom_J, Custom_G;
+    TextView txtJ, txtG, txtOX;
     EditText Content_Edit, FirstEdit, SecondEdit, ThirdEdit, ForthEdit;
+    Spinner spinnerCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +44,8 @@ public class MakeQuestionActivity extends AppCompatActivity {
         Custom_J_Blue = findViewById(R.id.custom_btn_j_blue);
         Custom_G_Blue = findViewById(R.id.custom_btn_g_blue);
         Custom_OX_Blue = findViewById(R.id.custom_btn_ox_blue);
+        spinnerCategory = findViewById(R.id.spinner_category);
 
-        txt_category = findViewById(R.id.custom_category);
         txtJ = findViewById(R.id.txt_j);
         txtG = findViewById(R.id.txt_g);
         txtOX = findViewById(R.id.txt_o);
@@ -53,7 +55,7 @@ public class MakeQuestionActivity extends AppCompatActivity {
         ThirdEdit = findViewById(R.id.custom_choice_edit3);
         ForthEdit = findViewById(R.id.custom_choice_edit4);
 
-        this.setBtn();
+        NewQuestionDto newQu = new NewQuestionDto();
 
         Custom_J.setOnClickListener(view -> {
             Custom_J.setVisibility(View.INVISIBLE);
@@ -61,12 +63,15 @@ public class MakeQuestionActivity extends AppCompatActivity {
             Custom_OX.setEnabled(false);
             Custom_G.setEnabled(false);
             Toast.makeText(view.getContext(), "주관식 선택!", Toast.LENGTH_SHORT).show();
+            newQu.setType("주관식");
         });
         Custom_J_Blue.setOnClickListener(view -> {
             Custom_J_Blue.setVisibility(View.INVISIBLE);
             Custom_J.setVisibility(View.VISIBLE);
             Custom_OX_Blue.setEnabled(true);
             Custom_G.setEnabled(true);
+            Toast.makeText(view.getContext(), "주관식 선택 취소!", Toast.LENGTH_SHORT).show();
+            newQu.setType("");
         });
         Custom_G.setOnClickListener(view -> {
             Custom_G.setVisibility(View.INVISIBLE);
@@ -74,12 +79,15 @@ public class MakeQuestionActivity extends AppCompatActivity {
             Custom_J.setEnabled(false);
             Custom_OX.setEnabled(false);
             Toast.makeText(view.getContext(), "객관식 선택!", Toast.LENGTH_SHORT).show();
+            newQu.setType("객관식");
         });
         Custom_G_Blue.setOnClickListener(view -> {
             Custom_G_Blue.setVisibility(View.INVISIBLE);
             Custom_G.setVisibility(View.VISIBLE);
             Custom_J.setEnabled(true);
             Custom_OX.setEnabled(true);
+            Toast.makeText(view.getContext(), "객관식 선택 취소!", Toast.LENGTH_SHORT).show();
+            newQu.setType("");
         });
         Custom_OX.setOnClickListener(view -> {
             Custom_OX.setVisibility(View.INVISIBLE);
@@ -87,38 +95,37 @@ public class MakeQuestionActivity extends AppCompatActivity {
             Custom_G.setEnabled(false);
             Custom_J.setEnabled(false);
             Toast.makeText(view.getContext(), "O X 선택!", Toast.LENGTH_SHORT).show();
+            newQu.setType("OX");
         });
         Custom_OX_Blue.setOnClickListener(view -> {
             Custom_OX_Blue.setVisibility(View.INVISIBLE);
             Custom_OX.setVisibility(View.VISIBLE);
             Custom_J.setEnabled(true);
             Custom_G.setEnabled(true);
+            Toast.makeText(view.getContext(), "O X 선택 취소!", Toast.LENGTH_SHORT).show();
+            newQu.setType("");
         });
 
-        Intent intent = getIntent();
-        if (intent != null && intent.getExtras() != null) {
-            String category = intent.getStringExtra("type");
-            txt_category.setText(category);
-        }
         CustomDone.setOnClickListener(view -> {
                     JsonObject jsonObject = new JsonObject();
                     jsonObject.addProperty("a", FirstEdit.getText().toString());
                     jsonObject.addProperty("b", SecondEdit.getText().toString());
                     jsonObject.addProperty("c", ThirdEdit.getText().toString());
                     jsonObject.addProperty("d", ForthEdit.getText().toString());
-                    NewQuestionDto newQu = new NewQuestionDto();
                     newQu.setContent(Content_Edit.getText().toString());
-                    newQu.setCategory(txt_category.getText().toString());
+                    newQu.setCategory(spinnerCategory.getSelectedItem().toString());
                     newQu.setChoices(jsonObject);
-                    newQu.setType(setBtn());
                     RetrofitHelper.getInstance().postQuestion(newQu, new Callback<NewQuestionDto>() {
                         @Override
                         public void onResponse(Call<NewQuestionDto> call, Response<NewQuestionDto> response) {
-                            Log.v("질문 직접 생성", String.valueOf(response.code()));
-                            Toast.makeText(MakeQuestionActivity.this, "질문 작성 완료!", Toast.LENGTH_SHORT).show();
-                            CustomDone.setBackgroundColor(Color.BLUE);
+                            if (response.isSuccessful()) {
+                                Log.v("질문 직접 생성", String.valueOf(response.code()));
+                                CustomDone.setBackgroundColor(Color.BLUE);
+                                launchQuestionActivity();
+                            } else if (response.code() == 400) {
+                                Toast.makeText(MakeQuestionActivity.this, "질문 세팅을 완료해주세요!", Toast.LENGTH_SHORT).show();
+                            }
                         }
-
                         @Override
                         public void onFailure(Call<NewQuestionDto> call, Throwable t) {
                             Log.v("질문 직접 생성 실패", t.getMessage());
@@ -128,39 +135,10 @@ public class MakeQuestionActivity extends AppCompatActivity {
         );
     }
 
-    public void onClick(View v) {
-        if (v.getId() == R.id.custom_btn_g) {
-        }
+    private void launchQuestionActivity() {
+        Toast.makeText(MakeQuestionActivity.this, "질문 작성 완료!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MakeQuestionActivity.this, QuestionActivity.class);
+        startActivity(intent);
+        finish();
     }
-
-
-    private String setBtn() {
-        View.OnClickListener listener = view -> {
-            switch (view.getId()) {
-                case R.id.custom_btn_g:
-                    Toast.makeText(MakeQuestionActivity.this, "ㅇㅇㅇㅇ", Toast.LENGTH_SHORT).show();
-                    newQu.setType("객관식");
-                    break;
-                case R.id.custom_btn_j:
-                    newQu.setType("주관식");
-                    break;
-                case R.id.custom_btn_ox:
-                    newQu.setType("OX");
-                    break;
-            }
-        };
-
-        Custom_J.setOnClickListener(listener);
-        Custom_G.setOnClickListener(listener);
-        Custom_OX.setOnClickListener(listener);
-
-
-//        Custom_J.setOnClickListener(view -> {
-//            Toast.makeText(MakeQuestionActivity.this, "ㅇㅇㅇㅇ", Toast.LENGTH_SHORT).show();
-//        });
-//        return txtJ.getText().toString();
-
-        return null;
-    }
-
 }
