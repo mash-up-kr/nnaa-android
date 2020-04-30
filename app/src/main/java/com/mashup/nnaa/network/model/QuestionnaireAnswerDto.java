f02e72ad18a68ce4cec08b770bee06a50d81bcee
SeungYooon <elvmaksrkwmdk@gmail.com>
2020-04-30 21:04:05 +0900

    modfiy questionnaire answer

--- app/src/main/java/com/mashup/nnaa/network/model/QuestionnaireAnswerDto.java
+++ app/src/main/java/com/mashup/nnaa/network/model/QuestionnaireAnswerDto.java
@@ -1,9 +1,34 @@
 package com.mashup.nnaa.network.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class QuestionnaireAnswerDto {
-    public String answeredAt;   //datetime
-    public String[] answers;
-    public Boolean CompleteFlag;
-    public String createdAt;    //datetime
-    public String updatedAt;    //datetime
+    @SerializedName("answers")
+    @Expose
+    private Answers answers;
+    @SerializedName("updatedAt")
+    @Expose
+    private String updatedAt;
+
+    public QuestionnaireAnswerDto(Answers answers, String updatedAt) {
+        this.answers = answers;
+        this.updatedAt = updatedAt;
+    }
+
+    public Answers getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Answers answers) {
+        this.answers = answers;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
