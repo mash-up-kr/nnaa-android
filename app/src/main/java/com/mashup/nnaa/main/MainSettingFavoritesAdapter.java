3eb7de5430848d6f31da8e526a61c799c9718bed
kohseungyoon <SeungYooon>
2020-05-07 19:05:15 +0900

    modfiy favorites in main setting

---
+++ app/src/main/java/com/mashup/nnaa/main/MainSettingFavoritesAdapter.java
@@ -0,0 +1,116 @@
+package com.mashup.nnaa.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mashup.nnaa.R;
+import com.mashup.nnaa.data.QuestionItem;
+import com.mashup.nnaa.network.RetrofitHelper;
+import com.mashup.nnaa.network.model.NewQuestionDto;
+import com.mashup.nnaa.network.model.Question;
+import com.mashup.nnaa.network.model.bookmarkQuestionDto;
+import com.mashup.nnaa.util.AccountManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class MainSettingFavoritesAdapter extends RecyclerView.Adapter<MainSettingFavoritesAdapter.ViewHolder> {
+
+    private ArrayList<NewQuestionDto> fList;
+    private Context fContext;
+    private String id = AccountManager.getInstance().getUserAuthHeaderInfo().getUserId();
+    private String token = AccountManager.getInstance().getUserAuthHeaderInfo().getToken();
+
+    public MainSettingFavoritesAdapter(Context context, ArrayList<NewQuestionDto> list) {
+        this.fList = list;
+        this.fContext = context;
+    }
+
+    public void setFavoritList(ArrayList<NewQuestionDto> fList) {
+        this.fList = fList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MainSettingFavoritesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main_setting_favorite, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainSettingFavoritesAdapter.ViewHolder holder, int position) {
+
+        holder.content_txt.setText(fList.get(position).getContent());
+
+        holder.q_txt.setOnClickListener(view -> {
+
+            int pos = holder.getAdapterPosition();
+            if (pos != RecyclerView.NO_POSITION) {
+                NewQuestionDto item = fList.get(pos);
+
+                RetrofitHelper.getInstance().favoriteDelete(id, token, item.getId(), new Callback<NewQuestionDto>() {
+                    @Override
+                    public void onResponse(Call<NewQuestionDto> call, Response<NewQuestionDto> response) {
+                        deleteFavorites(pos);
+                        Log.v("delete bookmark", "position" + pos);
+                    }
+
+                    @Override
+                    public void onFailure(Call<NewQuestionDto> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if (fList != null) {
+            return fList.size();
+        } else return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView q_txt, content_txt;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            q_txt = itemView.findViewById(R.id.q_text_msf);
+            content_txt = itemView.findViewById(R.id.question_text_msf);
+
+        }
+
+    }
+
+    private void deleteFavorites(int position) {
+        fList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, fList.size());
+    }
+}
